839b8a47ae597494ec7d5e5b50bab6d8e82265d6
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-29 22:59:32 +0200

    Zadanie 9

---
+++ src/Zadanie9.java
@@ -0,0 +1,14 @@
+public class Zadanie9 {
+    public static void main(String[] args) {
+
+        int[] liczby = new int[5];
+        liczby[0]= 20;
+        liczby[1]= 30;
+        liczby[2]= 40;
+        liczby[3]= 50;
+        liczby[4]= 60;
+        int sum = liczby[0]+liczby[2]+liczby[4];
+        System.out.println(sum);
+
+    }
+}
