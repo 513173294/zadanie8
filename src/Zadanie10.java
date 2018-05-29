e99281a2730308937d700f2b98d6ce4a82cb8603
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-30 00:06:13 +0200

    Zadanie 10

---
+++ src/Zadanie10.java
@@ -0,0 +1,16 @@
+public class Zadanie10 {
+    public static void main(String[] args) {
+        double i  = 0.0;
+        double a = 0.1 ;
+
+        if (i==0.0){
+            System.out.print(i + " ");}
+        while (i<= 3.0) {
+           System.out.printf("%.2f", i + 0.1 );
+            System.out.print(" ");
+
+            i = i+ 0.1;
+
+        }
+    }
+}
