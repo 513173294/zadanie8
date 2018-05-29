6d8d60ad3e7def239a54eb9488f0bc585d531318
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-30 00:25:36 +0200

    Zadanie 10 druga metoda

--- src/Zadanie10.java
+++ src/Zadanie10.java
@@ -1,16 +1,24 @@
 public class Zadanie10 {
     public static void main(String[] args) {
         double i  = 0.0;
-        double a = 0.1 ;
+//
+//        if (i==0.0){
+//            System.out.print(i + " ");}
+//        while (i<= 3.0) {
+//           System.out.printf("%.2f", i + 0.1 );
+//            System.out.print(" ");
+//
+//            i = i+ 0.1;
+//
+//        }
+        do {
+             i= i + 0.1;
+            System.out.printf("%.2f ", i);
 
-        if (i==0.0){
-            System.out.print(i + " ");}
-        while (i<= 3.0) {
-           System.out.printf("%.2f", i + 0.1 );
-            System.out.print(" ");
+        }while (i<=3);
 
-            i = i+ 0.1;
-
-        }
     }
+
+
+
 }
